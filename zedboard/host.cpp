c28364863b08f41abc78baa1ed996b3a1166a009
cds258 <cds258@cornell.edu>
2022-09-27 19:01:41 -0400

    host fix attempt 3

--- zedboard/host.cpp
+++ zedboard/host.cpp
@@ -105,14 +105,14 @@ int main(int argc, char** argv)
     //--------------------------------------------------------------------
     for (int i = 0; i < N; ++i ) {
 
-      int4_t digit_out;
+      int64_t digit_out;
+      bit64_t digit_i;
+      bit4_t result;
       nbytes = read (fdr, (void*)&digit_out, sizeof(digit_out));
-      assert (nbytes == sizeof(digit_out));
+      assert (nbytes == sizeof(result));
 
       // Convert int64 to fixed point 
-
-      bit4_t digit_i = digit_out;
-      bit4_t result;
+      digit_i = digit_out;
       result(result.length()-1,0) = digit_i(result.length()-1,0);
 
       num_test_insts++;
